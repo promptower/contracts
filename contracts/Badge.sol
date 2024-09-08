27f248473b574ecb1710892e85e2b5b42a067038
lukepark327 <lukepark327@gmail.com>
2024-09-08 13:39:14 +0900

    chore: temporal assets

---
+++ contracts/Badge.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Verified is ERC721("Verified Badge", "Verified"), Ownable {
+    uint256 public totalSupply;
+
+    constructor() Ownable(msg.sender) {}
+
+    function mint(address to) external onlyOwner {
+        _mint(to, totalSupply++);
+    }
+}
